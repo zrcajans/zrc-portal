4f99cd2bc913ade42fe47b2bffc022357b84f840
Enes Zariç <enszrc@gmail.com>
2026-06-08 03:16:27 +0300

    mobil uygulama kimligi v283

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import TaskModal from './components/Modals/TaskModal';
 import StageModal from './components/Modals/StageModal';
 import { supabase } from './supabaseClient';
 
-const ZRC_APP_BUILD_LABEL = 'v282-ust-bar-mobil-sigma';
+const ZRC_APP_BUILD_LABEL = 'v283-mobil-uygulama-kimligi';
 
 class ZRCErrorBoundary extends React.Component {
   constructor(props) {
