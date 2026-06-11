6db2002b5093d4b0148b288edc94adb09bec3832
Enes Zariç <enszrc@gmail.com>
2026-06-11 20:04:31 +0300

    mobil bildirim paneli tasma duzeltmesi v384-safe

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TaskModal from './components/Modals/TaskModal';
 import StageModal from './components/Modals/StageModal';
 import { supabase } from './supabaseClient';
 
-const ZRC_APP_BUILD_LABEL = 'v383b-safe-stale-task-clean';
+const ZRC_APP_BUILD_LABEL = 'v384-safe-mobile-notification-panel';
 
 class ZRCErrorBoundary extends React.Component {
   constructor(props) {
