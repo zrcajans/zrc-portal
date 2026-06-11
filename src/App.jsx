c54559908927d6f375a3dd7083c7f80bf7da4209
Enes Zariç <enszrc@gmail.com>
2026-06-11 18:10:43 +0300

    mobil form ve liste rahatlatma v365-safe

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TaskModal from './components/Modals/TaskModal';
 import StageModal from './components/Modals/StageModal';
 import { supabase } from './supabaseClient';
 
-const ZRC_APP_BUILD_LABEL = 'v364-safe-mobile-panels';
+const ZRC_APP_BUILD_LABEL = 'v365-safe-mobile-forms-lists';
 
 class ZRCErrorBoundary extends React.Component {
   constructor(props) {
