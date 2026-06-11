79f7bdc44c09152f15831fa46a4811e14877e950
Enes Zariç <enszrc@gmail.com>
2026-06-11 21:35:50 +0300

    web sol ust logo mudahalesini geri al v396-safe

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TaskModal from './components/Modals/TaskModal';
 import StageModal from './components/Modals/StageModal';
 import { supabase } from './supabaseClient';
 
-const ZRC_APP_BUILD_LABEL = 'v395b-safe-existing-logo-visible';
+const ZRC_APP_BUILD_LABEL = 'v396-safe-web-sidebar-logo-rollback';
 
 class ZRCErrorBoundary extends React.Component {
   constructor(props) {
