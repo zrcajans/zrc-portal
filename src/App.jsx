aaf392fb1f85482caaea52e8a5936b81c16c7000
Enes Zariç <enszrc@gmail.com>
2026-06-11 21:15:47 +0300

    mobil logo ve pastel secili proje v393-safe

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TaskModal from './components/Modals/TaskModal';
 import StageModal from './components/Modals/StageModal';
 import { supabase } from './supabaseClient';
 
-const ZRC_APP_BUILD_LABEL = 'v392-safe-mobile-project-picker-design';
+const ZRC_APP_BUILD_LABEL = 'v393-safe-mobile-logo-pastel-branding';
 
 class ZRCErrorBoundary extends React.Component {
   constructor(props) {
