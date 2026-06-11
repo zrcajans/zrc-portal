ce5760ac6956c96c91595127a473e3ed42b3e1d8
Enes Zariç <enszrc@gmail.com>
2026-06-11 20:08:51 +0300

    mobil panel baglanti tasarimini geri al v386-safe

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TaskModal from './components/Modals/TaskModal';
 import StageModal from './components/Modals/StageModal';
 import { supabase } from './supabaseClient';
 
-const ZRC_APP_BUILD_LABEL = 'v385-safe-mobile-connected-panels';
+const ZRC_APP_BUILD_LABEL = 'v384-safe-mobile-notification-panel';
 
 class ZRCErrorBoundary extends React.Component {
   constructor(props) {
