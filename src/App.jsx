555417d8dda7a76cc4f2de91d3a2b9995f65108a
Enes Zariç <enszrc@gmail.com>
2026-06-11 17:23:22 +0300

    surum ve guncelleme uyarilarini temizle v350-safe

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import TaskModal from './components/Modals/TaskModal';
 import StageModal from './components/Modals/StageModal';
 import { supabase } from './supabaseClient';
 
-const ZRC_APP_BUILD_LABEL = 'v349-safe-debug-rozet-temizlik';
+const ZRC_APP_BUILD_LABEL = 'v350-safe-surum-uyari-temizlik';
 
 class ZRCErrorBoundary extends React.Component {
   constructor(props) {
