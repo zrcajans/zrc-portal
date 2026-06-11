f31e18ad3323e94a2f347386aeb687d7d8938f5a
Enes Zariç <enszrc@gmail.com>
2026-06-11 19:11:07 +0300

    mobil yatay tasma duzeltmesi v372-safe

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TaskModal from './components/Modals/TaskModal';
 import StageModal from './components/Modals/StageModal';
 import { supabase } from './supabaseClient';
 
-const ZRC_APP_BUILD_LABEL = 'v371-safe-mobile-minimal-flow';
+const ZRC_APP_BUILD_LABEL = 'v372-safe-mobile-overflow-fix';
 
 class ZRCErrorBoundary extends React.Component {
   constructor(props) {
