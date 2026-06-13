c0bfe053584b809f1ded9cdd90b2377fd8721675
Enes Zariç <enszrc@gmail.com>
2026-06-13 14:56:02 +0300

    mobil push test panelini kaldir v445-safe

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TaskModal from './components/Modals/TaskModal';
 import StageModal from './components/Modals/StageModal';
 import { supabase } from './supabaseClient';
 
-const ZRC_APP_BUILD_LABEL = 'v442-safe-single-direct-task-push';
+const ZRC_APP_BUILD_LABEL = 'v445-safe-push-panel-removed';
 
 class ZRCErrorBoundary extends React.Component {
   constructor(props) {
